b67658c5fcb52a5e78b786d7c005da14c0fd19c4
Murilo Nogueira <51790787+Murilo831@users.noreply.github.com>
2023-11-03 10:17:45 -0300

    CSS to toggle scroll axis styles index

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("overlay").style.display = "none";
 }
+
+/*
+ JS to toggle scroll axis styles
+*/
+const control = document.getElementById("direction-toggle");
+const marquees = document.querySelectorAll(".marquee");
+const wrapper = document.querySelector(".wrapper");
+
+control.addEventListener("click", () => {
+  control.classList.toggle("toggle--vertical");
+  wrapper.classList.toggle("wrapper--vertical");
+  [...marquees].forEach((marquee) =>
+    marquee.classList.toggle("marquee--vertical")
+  );
+});
