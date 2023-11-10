c9835329b75eb0e9bce4296508bda7e7111ca4ff
Murilo Nogueira <51790787+Murilo831@users.noreply.github.com>
2023-11-10 15:00:13 -0300

    update

--- js/script.js
+++ js/script.js
@@ -16,26 +16,82 @@ document.addEventListener("DOMContentLoaded", function() {
   var codeElement = document.getElementById('code');
   if (!codeElement) return; // Se o elemento não existir, saia da função
 
-  var code = `// Código de exemplo
-function sayHello() {
-  console.log('Hello, world!');
-}
+  var codes = [
+    `// Django - Código de Rota Básica
+from django.http import HttpResponse
 
-sayHello();`;
+def home(request):
+    return HttpResponse("Olá, mundo do Django!")`,
 
+    `# Python - Data Science com Pandas
+import pandas as pd
+
+# Carregando e exibindo os primeiros dados do dataset
+df = pd.read_csv('data.csv')
+print(df.head())`,
+
+    `# Python - Machine Learning com scikit-learn
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+# Carregando o dataset Iris e criando o modelo
+iris = load_iris()
+X, y = iris.data, iris.target
+clf = DecisionTreeClassifier()
+clf.fit(X, y)
+
+# Fazendo uma previsão
+print(clf.predict([X[0]]))`,
+
+    `# Python - NLP com NLTK
+import nltk
+from nltk.tokenize import word_tokenize
+
+# Exemplo de tokenização de palavras
+frase = "Olá, mundo do NLP!"
+tokens = word_tokenize(frase)
+print(tokens)`,
+
+    `// Kotlin - Código Básico para App Mobile
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = "Olá, mundo do Kotlin!"
+    }
+}`
+];
+
+  var current = 0;
   var i = 0;
-  var speed = 50; // Velocidade de digitação em milissegundos
+  var speed = 30; // Velocidade de digitação em milissegundos
 
   function typeCode() {
-      if (i < code.length) {
-          codeElement.innerHTML += code.charAt(i);
+      if (i < codes[current].length) {
+          codeElement.innerHTML += codes[current].charAt(i);
           i++;
           setTimeout(typeCode, speed);
+      } else {
+          setTimeout(nextCode, 3000); // Espera 3 segundos antes de mudar para o próximo código
       }
   }
 
-  typeCode(); // Inicia a função de digitação
+  function nextCode() {
+      current = (current + 1) % codes.length; // Move para o próximo código ou volta ao primeiro
+      codeElement.innerHTML = ''; // Limpa o container de código
+      i = 0; // Reinicia o índice para o novo código
+      typeCode(); // Começa a digitar o novo código
+  }
+
+  typeCode(); // Inicia a digitação do primeiro código
 });
+
 
 /*
  JS to toggle scroll axis styles
